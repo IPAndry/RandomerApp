636b2d713a8077b4ac9f4978c4029c9de38626a1
Andrew Bykov <31726841+IPAndry@users.noreply.github.com>
2020-10-01 18:07:15 +0300

    Publish

---
+++ Randomer/Assets/Scripts/ChangeCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeCamera : MonoBehaviour, IPointerClickHandler
+{
+    // Start is called before the first frame update
+    [SerializeField] Camera FirstCam;
+    [SerializeField] Camera SecondCam;
+
+    [SerializeField] GameObject FirstCanvas;
+    [SerializeField] GameObject SecondCanvas;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        FirstCam.gameObject.SetActive(false);
+        SecondCam.gameObject.SetActive(true);
+
+        FirstCanvas.gameObject.SetActive(false);
+        SecondCanvas.gameObject.SetActive(true);
+    }
+}
